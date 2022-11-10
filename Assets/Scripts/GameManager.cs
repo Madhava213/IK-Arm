1334054b708cfa806b37031f4268739359a4f5ba
Madhava R <51475184+Madhava213@users.noreply.github.com>
2022-11-09 20:59:18 -0600

    Inverse Kinematics implemented without constraints

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,8 +13,10 @@ public class GameManager : MonoBehaviour
     private GameObject[] Joints;
     public int numArms = 5;
     private int numJoints;
+    private float armLength = 0;
 
     public Transform TargetPos;
+    private Vector3 lastTargetPos;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +32,16 @@ public class GameManager : MonoBehaviour
         Groups = new GameObject[numArms];
         Joints = new GameObject[numJoints];
 
+        // Arm Length
+        armLength = Arm.transform.localScale.z;
+
+        // Instantiate lastTargetPos
+        lastTargetPos = new Vector3(0, 0, 0);
+
+        // Spawn
+        foreach (Transform child in Base.transform) {
+            GameObject.Destroy(child.gameObject);
+        }
         SpawnCharacter();
     }
 
@@ -48,18 +60,20 @@ public class GameManager : MonoBehaviour
             Groups.SetValue(emptyObj,i);
 
             // Group position at Joint's / Rotation Offset
-            emptyObj.transform.position = new Vector3(0, 0 + ((i) * (Arm.transform.localScale.y)), 0);
-            emptyObj.transform.Rotate(-90, 0, 0);
+            emptyObj.transform.position = new Vector3(0, 0 + ((i+1) * (armLength)), 0);
+            // emptyObj.transform.Rotate(-90, 0, 0);
+            emptyObj.transform.LookAt(TargetPos);
 
             // Create Arm
-            GameObject newArm = Instantiate(Arm, new Vector3(0,0 + baseRadius + (i * (Arm.transform.localScale.y)),0), Quaternion.identity);
+            GameObject newArm = Instantiate(Arm, new Vector3(0,0 + baseRadius + (i * (armLength)),0), Quaternion.identity);
+            newArm.transform.Rotate(90, 0, 0);
             newArm.transform.SetParent(emptyObj.transform);
             Arms.SetValue(newArm, i);
 
 
             // Create Joint
             if(i < numJoints){
-                GameObject newJoint = Instantiate(Joint, new Vector3(0,0 + ((i+1) * (Arm.transform.localScale.y)),0), Quaternion.identity);
+                GameObject newJoint = Instantiate(Joint, new Vector3(0,0 + ((i+1) * (armLength)),0), Quaternion.identity);
                 newJoint.transform.SetParent(emptyObj.transform);
                 Joints.SetValue(newJoint, i);
             }
@@ -78,38 +92,72 @@ public class GameManager : MonoBehaviour
         // Groups[2].transform.position += new Vector3(x * speed * Time.deltaTime,0,z * speed * Time.deltaTime);
         //////////////////////////////////////
 
-        // Get all Positions
-        Vector3[] GroupPositions = new Vector3[Groups.Length];
-        for (var i = 0; i < Groups.Length; i++)
+        if(TargetPos.position.Equals(lastTargetPos)){
+            FabrikIK();
+        }
+        lastTargetPos = TargetPos.position;
+    }
+
+    void FabrikIK(){
+        for (int z = 0; z < 20; z++)
         {
-            GroupPositions.SetValue(Groups[i].transform.position, i);
-        }
-
-        // Backward
-        GroupPositions[GroupPositions.Length-1] = TargetPos.position;
-        for (int i = GroupPositions.Length - 2; i >= 0; i--) {
-            Vector3 dir = (GroupPositions[i + 1] - GroupPositions[i]).normalized;
-            GroupPositions[i] = GroupPositions[i + 1] + dir * Arm.transform.localScale.y;
-        }
-
-        // Forward
-        GroupPositions[0] = Base.transform.position;
-        for (int i = 1; i < numArms; i++) {
-            Vector3 dir = (GroupPositions[i] - GroupPositions[i-1]).normalized;
-            GroupPositions[i] = GroupPositions[i-1] + dir * Arm.transform.localScale.y;
-        }
-
-        // Reassign Positions
-        for (var i = 0; i < Groups.Length; i++)
-        {
-            Groups[i].transform.position = GroupPositions[i];
-            if(i == Groups.Length-1){
-                Groups[i].transform.LookAt(TargetPos);
+	        // Get all Positions
+            Vector3[] GroupPositions = new Vector3[Groups.Length];
+            for (var i = 0; i < Groups.Length; i++)
+            {
+                GroupPositions.SetValue(Groups[i].transform.position, i);
             }
-            else{
-                Groups[i].transform.LookAt(Groups[i+1].transform);
+	
+	        // Backward
+            GroupPositions[GroupPositions.Length-1] = TargetPos.position;
+            for (int i = GroupPositions.Length - 2; i >= 0; i--) {
+                Vector3 dir = (GroupPositions[i + 1] - GroupPositions[i]).normalized;
+                GroupPositions[i] = GroupPositions[i + 1] + dir * armLength;
             }
+	
+	        // Forward
+            for (int i = 0; i < numArms; i++) {
+                if(i == 0){
+                    Vector3 dir = (GroupPositions[i] - Base.transform.position).normalized;
+                    GroupPositions[i] = Base.transform.position + dir * armLength;
+                }
+                else{
+                    Vector3 dir = (GroupPositions[i] - GroupPositions[i-1]).normalized;
+                    GroupPositions[i] = GroupPositions[i-1] + dir * armLength;
+                }
+            }
+
+
+            // Reassign Positions
+            for (var i = 0; i < Groups.Length; i++)
+            {
+                Groups[i].transform.position = GroupPositions[i];
+
+                if(i == Groups.Length-1){
+                    Groups[i].transform.LookAt(TargetPos);
+
+                    // Arms Reset
+                    Vector3 armDir = (Groups[i].transform.position - Groups[i-1].transform.position) / 2.0f;
+                    Arms[i].transform.position = Groups[i-1].transform.position + armDir;
+                    Arms[i].transform.LookAt(Groups[i].transform.position);
+                }
+                else if(i == 0){
+                    // Arms Reset
+                    Vector3 armDir = (Groups[i].transform.position - Base.transform.position) / 2.0f;
+                    Arms[i].transform.position = Base.transform.position + armDir;
+                    Arms[i].transform.LookAt(Groups[i].transform.position);
+                }
+                else{
+                    Groups[i].transform.LookAt(Groups[i+1].transform);
+
+                    // Arms Reset
+                    Vector3 armDir = (Groups[i].transform.position - Groups[i-1].transform.position) / 2.0f;
+                    Arms[i].transform.position = Groups[i-1].transform.position + armDir;
+                    Arms[i].transform.LookAt(Groups[i].transform.position);
+                }
+                
+            }
+
         }
-        
     }
 }
